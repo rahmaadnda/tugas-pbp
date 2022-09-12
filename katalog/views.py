7b5b0918a86381e86ba26f6a1f1b5ba296649524
Rahma Adinda Putri <putrirahmaadinda@gmail.com>
2022-09-12 19:42:50 +0700

    tugas 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+data_catalog_item = CatalogItem.objects.all()
+context = {
+    'list_barang': data_catalog_item,
+    'nama': 'Rahma Adinda',
+    'ID': '2106750774'
+}
+
+def show_katalog(request):
+    return render(request, "katalog.html", context)
